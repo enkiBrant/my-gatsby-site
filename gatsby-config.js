0b5a09a87f65087352538610387c758e64ca55de
EnkiBrant <enki.brant@gmail.com>
2024-10-10 21:29:05 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,19 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "Illustrrr",
+    description: "L'illustration dans tous ses états",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
